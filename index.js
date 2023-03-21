2df38e268723126d503dff85a72fcc2f29747cf0
Jeevan Johal <jkjohal@ucdavis.edu>
2023-03-20 22:02:29 -0700

    initialization of app

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const { prompt } = require("inquirer");
 const db = require("./db");
 require("console.table");
+const logo = require("asciiart-logo");
 
+function init() {
+    const logoText = logo({ name: "Employee Manager" }).render();
+  
+    console.log(logoText);
+  
+    loadMainPrompts();
+  };
+
+init();  
